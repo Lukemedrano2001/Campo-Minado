4f1213a607bc543b34bb0fd7ae4ed4ad9dfd131d
Lukemedrano2001 <144495981+Lukemedrano2001@users.noreply.github.com>
2024-01-05 09:42:13 -0300

    Update BotaoCampo.java

--- src/br/com/lukemedrano/campoMinado/view/BotaoCampo.java
+++ src/br/com/lukemedrano/campoMinado/view/BotaoCampo.java
@@ -13,24 +13,11 @@ import br.com.lukemedrano.campoMinado.model.campo.ObserverCampo;
 
 @SuppressWarnings("serial")
 public class BotaoCampo extends JButton implements ObserverCampo, MouseListener {
-	private final Color BG_PADRAO_CINZA = new Color(160, 160, 160);
-	private final Color BG_ABERTO = new Color(180, 180, 180);
-	private final Color BG_MARCADO = new Color(8, 179, 247);
-	private final Color BG_EXPLODIR = new Color(189, 66, 68);
-	
-	private final Color BORDA = new Color(60, 60, 60);
-	private final Color TEXTO_VERDE = new Color(0, 150, 0);
-	private final Color TEXTO_AZUL = new Color(0, 0, 255);
-	private final Color TEXTO_AMARELO = new Color(255, 255, 0);
-	private final Color TEXTO_VERMELHO = new Color(255, 0, 0);
-	private final Color TEXTO_ROSA = new Color(255, 80, 160);
-	
-	
 	private Campo campo;
 	
 	public BotaoCampo(Campo campo) {
 		this.campo = campo;
-		setBackground(BG_PADRAO_CINZA);
+		setBackground(new Color(160, 160, 160));
 		setOpaque(true);
 		setBorder(BorderFactory.createBevelBorder(0));
 		
@@ -65,31 +52,31 @@ public class BotaoCampo extends JButton implements ObserverCampo, MouseListener 
 	}
 
 	private void aplicaEstiloAbrir() {
-		if(this.campo.isMinado()) {
-			setBackground(BG_EXPLODIR);
+		if(this.getCampo().isMinado()) {
+			setBackground(new Color(189, 66, 68));
 			return;
 		}
 		
-		setBackground(BG_ABERTO);
-		setBorder(BorderFactory.createLineBorder(BORDA));
+		setBackground(new Color(180, 180, 180));
+		setBorder(BorderFactory.createLineBorder(new Color(60, 60, 60)));
 		
 		switch(this.campo.minasNaVizinhaca()) {
 			case 1:
-				setForeground(TEXTO_VERDE);
+				setForeground(new Color(0, 150, 0));
 				break;
 			case 2:
-				setForeground(TEXTO_AZUL);
+				setForeground(new Color(0, 0, 255));
 				break;
 			case 3:
-				setForeground(TEXTO_AMARELO);
+				setForeground(new Color(255, 255, 0));
 				break;
 			case 4:
 			case 5:
 			case 6:
-				setForeground(TEXTO_VERMELHO);
+				setForeground(new Color(255, 0, 0));
 				break;
 			default:
-				setForeground(TEXTO_ROSA);
+				setForeground(new Color(255, 80, 160));
 		}
 		
 		String valor = this.getCampo().vizinhacaPerigosa() ? this.getCampo().minasNaVizinhaca() + "" : "";
@@ -97,19 +84,19 @@ public class BotaoCampo extends JButton implements ObserverCampo, MouseListener 
 	}
 	
 	private void aplicaEstiloMarcar() {
-		setBackground(BG_MARCADO);
+		setBackground(new Color(8, 179, 247));
 		setForeground(new Color(0, 0, 0));
 		setText("M");
 	}
 
 	private void aplicaEstiloExplodir() {
-		setBackground(BG_EXPLODIR);
+		setBackground(new Color(189, 66, 68));
 		setForeground(new Color(255, 255, 255));
 		setText("X");
 	}
 	
 	private void aplicaEstiloPadrao() {
-		setBackground(BG_PADRAO_CINZA);
+		setBackground(new Color(160, 160, 160));
 		setBorder(BorderFactory.createBevelBorder(0));
 		setText("");
 	}
